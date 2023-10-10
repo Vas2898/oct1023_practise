74d66ede88d4e726e43403ff769549f9a34d30cd
vasanthakumar <109866822+Vas2898@users.noreply.github.com>
2023-10-10 12:27:46 +0530

    oct1023practise.java

---
+++ oct1023practise.java
@@ -0,0 +1,15 @@
+public class ipl2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean rcp_trophy = false;
+		if (rcp_trophy == true) {
+			System.out.println("Get married soon");		}
+		else {
+			System.out.println("better luck next year");
+		}
+
+	}
+
+}
